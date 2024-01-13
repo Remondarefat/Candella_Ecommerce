a2bb73780a4bc8cd62cf3899579fd6a24783d4ac
Mina Naguib <132841396+minanagib112@users.noreply.github.com>
2024-01-13 18:03:22 +0200

    mina-branch edit

--- js/main.js
+++ js/main.js
@@ -1,70 +1,112 @@
-var allItems = [];
-
-(function () {
-  async function getProducts(category) {
-    var res = await fetch(`https://dummyjson.com/products/${category}`);
-    var data = await res.json();
-    var productsArray = data.products;
-    allItems = productsArray;
-    console.log(productsArray);
-    displayProduct(allItems);
-  }
-
-  window.onload = function () {
-    getProducts("");
-  };
-  var category = document.querySelectorAll(".category");
-  for (var i = 0; i < category.length; i++) {
-    category[i].addEventListener("click", function (eventInfo) {
-      getProducts(eventInfo.target.getAttribute("data-category"));
-    });
-  }
-  function displayProduct() {
-    var container = "";
-    for (var i = 0; i < allItems.length; i++) {
-      container += `
-        <div  class=" col-md-3 card ">
-          <img class="w-25" src="${allItems[i].thumbnail}"/>
-          <div class="col-md-8">
-          <div class="item">
-              <h2>${allItems[i].title}</h2>
-              <p>${allItems[i].price} $</p>
-              <p class="d-none">${allItems[i].category}</p>
-              
-          </div>
-          </div>
-          </div>`;
-    }
-    document.getElementById("rowData").innerHTML = container;
-  }
-
-  var links = document.querySelectorAll(".category");
-
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-      var activeLink = document.querySelector(".category.active");
-      if (activeLink) {
-        activeLink.classList.remove("active");
+(function(){
+  let img_slider = document.getElementsByClassName('img_slider');
+  let step = 0;
+  let nbr_img = img_slider.length;
+  let prev = document.querySelector('.prev');
+  let next = document.querySelector('.next');
+  
+  function deleteActiveImages(){
+      for(let i = 0 ; i < nbr_img ; i++){
+          img_slider[i].classList.remove('active');
       }
-      this.classList.add("active");
-    });
   }
-  function scrollToTop() {
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-  }
-  var button = document.querySelector(".top-button");
-  button.addEventListener("click", scrollToTop);
-  function toggleButton() {
-    let scrollPos =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    let threshold = 100;
-    if (scrollPos > threshold) {
-      button.style.display = "block";
-    } else {
-      button.style.display = "none";
-    }
-  }
-
-  window.addEventListener("scroll", toggleButton);
+  
+  next.addEventListener('click',function(){
+      step++;
+      if(step >= nbr_img){
+          step = 0;
+      }
+      deleteActiveImages();
+      img_slider[step].classList.add('active');
+  });
+  prev.addEventListener('click',function(){
+      step--;
+      if(step < 0){
+          step = nbr_img -1;
+      }
+      deleteActiveImages();
+      img_slider[step].classList.add('active');
+  });
+  setInterval(function(){
+      step++;
+      if(step >= nbr_img){
+          step = 0;
+      }
+      deleteActiveImages();
+      img_slider[step].classList.add('active');
+  
+  }, 3000);
 })();
+
+
+  var allItems = [];
+
+  (function () {
+    async function getProducts(category) {
+      var res = await fetch(`https://dummyjson.com/products/${category}`);
+      var data = await res.json();
+      var productsArray = data.products;
+      allItems = productsArray;
+      console.log(productsArray);
+      displayProduct(allItems);
+    }
+  
+    window.onload = function () {
+      getProducts("");
+    };
+    var category = document.querySelectorAll(".category");
+    for (var i = 0; i < category.length; i++) {
+      category[i].addEventListener("click", function (eventInfo) {
+        getProducts(eventInfo.target.getAttribute("data-category"));
+        eventInfo.preventDefault();
+      });
+    }
+    function displayProduct() {
+      var container = "";
+      for (var i = 0; i < allItems.length; i++) {
+        container += `
+          <div  class=" col-md-3 card ">
+            <img class="w-25" src="${allItems[i].thumbnail}"/>
+            <div class="col-md-8">
+            <div class="item">
+                <h2>${allItems[i].title}</h2>
+                <p>${allItems[i].price} $</p>
+                <p class="d-none">${allItems[i].category}</p>
+                
+            </div>
+            </div>
+            </div>`;
+      }
+      document.getElementById("rowData").innerHTML = container;
+    }
+  
+    var links = document.querySelectorAll(".category");
+  
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", function () {
+        var activeLink = document.querySelector(".category.Active");
+        if (activeLink) {
+          activeLink.classList.remove("Active");
+        }
+        this.classList.add("Active");
+      });
+    }
+    function scrollToTop() {
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+    }
+    var button = document.querySelector(".top-button");
+    button.addEventListener("click", scrollToTop);
+    function toggleButton() {
+      let scrollPos =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      let threshold = 100;
+      if (scrollPos > threshold) {
+        button.style.display = "block";
+      } else {
+        button.style.display = "none";
+      }
+    }
+  
+    window.addEventListener("scroll", toggleButton);
+  })();
